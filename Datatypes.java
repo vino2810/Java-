87fcf738a68278b56a5616841cdd8849454bc9ff
vino2810 <151716575+vino2810@users.noreply.github.com>
2024-01-26 13:20:46 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,25 @@
+package java_practice;
+
+public class Datatypes {
+	public static void main(String args[]) {
+		int a=4;
+		byte b=1;
+		short c=2;
+		long d=8;
+		float e=4.0f;
+		double f=8.0;
+		boolean value = true;
+		char word = 's';
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(value);
+		System.out.println(word);
+		
+	}
+
+}
